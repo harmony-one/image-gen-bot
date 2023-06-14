35d89ba6844e6dc4d631892dab0b133cbbc9d05a
fegloff <fegloff@gmail.com>
2023-06-14 17:29:55 -0500

    add start command and fly.io configuration

--- src/bot.ts
+++ src/bot.ts
@@ -3,23 +3,29 @@ import { improvePrompt, postGenerateImg } from "./api/openAi";
 import { config } from "./config";
 import express from "express";
 
-console.log('CONFIG',config)
+console.log('CONFIG',config.isProduction, config.port, config.completions)
 
 const token = config.telegramAPI;
 const bot = new Bot(token || "");
+const welcomeText = `Hello! I'm a Telegram bot that generates AI Images using OpenAI technology.
+
+<b>Commands</b>
+/help - This menu
+/gen [text] - Generates an Image from a given prompt
+/genEn [text] - Generates an Image from an enhanced prompt`;
+
+bot.command("start", (ctx) => {
+  console.log('start command')
+  ctx.reply(welcomeText, { parse_mode: "HTML" });
+});
 
 bot.command("help", (ctx) => {
-  const helpText = `Hello! I'm a Telegram bot that generates AI Images using OpenAI technology.
-
-  <b>Commands</b>
-  /help - This menu
-  /gen [text] - Generates an Image from a given prompt
-  /genEn [text] - Generates an Image from an enhanced prompt`;
-
-  ctx.reply(helpText, { parse_mode: "HTML" });
+  console.log('help command')
+  ctx.reply(welcomeText, { parse_mode: "HTML" });
 });
 
 bot.command("gen", async (ctx) => {
+  console.log('gen command')
   const prompt = ctx.match;
   if (!prompt) {
     ctx.reply("Error: Missing prompt");
@@ -38,6 +44,7 @@ bot.command("gen", async (ctx) => {
 });
 
 bot.command("genEn", async (ctx) => {
+  console.log('genEn command')
   const prompt = ctx.match;
   if (!prompt) {
     ctx.reply("Error: Missing prompt");
@@ -70,7 +77,7 @@ if (config.isProduction) {
   app.use(webhookCallback(bot, "express"));
 
   const PORT = config.port;
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Bot listening on port ${PORT}`);
   });
 } else {
